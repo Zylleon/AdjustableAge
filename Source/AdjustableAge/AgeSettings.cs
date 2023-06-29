ea12388d211914e181e7721f4fcab459fd08930a
Zylleon <56230821+Zylleon@users.noreply.github.com>
2023-06-29 17:56:45 -0600

    initial commit

---
+++ Source/AdjustableAge/AgeSettings.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace AdjustableAge
+{
+    public class AgeSettings : ModSettings
+    {
+        public IntRange allowedAges = new IntRange(15, 80);
+
+
+        /// <summary>
+        /// The part that writes our settings to file. Note that saving is by ref.
+        /// </summary>
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref allowedAges, "allowedAges", new IntRange(15, 80));
+        }
+    }
+
+
+    public class AgeMod : Mod
+    {
+        public AgeSettings settings;
+
+        public AgeMod(ModContentPack content) : base(content)
+        {
+            this.settings = GetSettings<AgeSettings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+
+            //inRect.width = 450f;
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+            listingStandard.Label("ZAA_ageLabel".Translate());
+
+            Rect ageRangeRect = listingStandard.GetRect(35);
+            Widgets.IntRange(ageRangeRect, 130, ref settings.allowedAges, 0, 150);
+
+            listingStandard.Gap();
+            if (settings.allowedAges.min < 13)
+            {
+                GUI.color = new Color(255, 180, 0);
+                listingStandard.Label("ZAA_ageWarning".Translate());
+                GUI.color = Color.white;
+            }
+
+            listingStandard.End();
+
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return "ZAA_ModName".Translate();
+        }
+
+    }
+
+
+
+
+}
---
+++ Source/AdjustableAge/Patches.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace AdjustableAge
+{
+    [StaticConstructorOnStartup]
+    static class Patches
+    {
+        static Patches()
+        {
+            Harmony harmony = new Harmony("zylle.AdjustableAge");
+            harmony.PatchAll();
+            Log.Message("Adjustable Age initialized");
+        }
+    }
+
+
+    [HarmonyPatch(typeof(Verse.PawnGenerator), "GenerateRandomAge")]
+    static class AgePatch
+    {
+        static bool Prefix(Pawn pawn, ref PawnGenerationRequest request)
+        {
+
+            if (pawn.RaceProps.Humanlike && !request.AllowedDevelopmentalStages.Newborn())
+            {
+                IntRange ages = LoadedModManager.GetMod<AgeMod>().GetSettings<AgeSettings>().allowedAges;
+
+                request.FixedBiologicalAge = Rand.Range(ages.min, ages.max);
+
+            }
+
+            return true;
+        }
+       
+    }
+
+}
